126133645173f66a2cecfe3ceecfed05fb0ea2bd
fym202 <2285010001@qq.com>
2023-12-22 10:25:23 +0800

    add compare analyse

--- analyse.py
+++ analyse.py
@@ -147,6 +147,8 @@ def count_word_frequency(paper_list_roots, stop_words_root, save_root,
                          plot_wordcloud=True):
     # 进行词频统计
     frequency = paper_title_word_frequency(paper_list_roots, stop_words_root)
+    print("\nTop 20 high-frequency words in paper titles.\n")
+    print(list(frequency.items())[:20])
     # 保存词频到excel中
     save_word_frequency(frequency, save_root, frequencies_filename)
     # 绘制直方图
@@ -156,8 +158,87 @@ def count_word_frequency(paper_list_roots, stop_words_root, save_root,
         generate_wordcloud(frequency, save_root, wordcloud_filename)
 
 
+# 得到单词的排名
+def get_rank(word_freq, rank_cut = 200):
+    ranks = np.empty_like(word_freq)
+    ranks[0] = 1        # 最前面的排第一
+    temp_rank = 1
+    for i in range(1, word_freq.shape[0]):
+        if word_freq[i-1] == word_freq[i]:
+            ranks[i] = temp_rank
+        else:
+            temp_rank = i + 1
+            if temp_rank >= 200:
+                ranks[i:-1] = 200
+                return ranks
+            ranks[i] = temp_rank
+    return ranks
+
+
+# 存储到字典中
+def store_ranks(words, ranks):
+    save_dict = {}
+    for word, rank in zip(words, ranks):
+        save_dict[word] = rank
+    return save_dict
+
+
+# 保存排名变化到excel文件中
+def save_rank_change(words, pre_post_rank, post_rank, rank_change,
+                     save_root, filename):
+    """
+    words: 单词
+    pre_post_rank: 单词对应的上一次的排面
+    post_rank: 单词对应本次的排名
+    rank_change: 单词本次相比上一次的变化
+    """
+    change_dict = {
+        "Word": words,
+        "PreRank": pre_post_rank,
+        "PostRank": post_rank,
+        "Change": rank_change
+    }
+    df = pd.DataFrame(change_dict)
+    if filename[-5:] != ".xlsx":
+        filename = filename + ".xlsx"
+    save_filename = os.path.join(save_root, filename)
+    print("Result of this compare save: " + save_filename)
+    df.to_excel(save_filename, index=False)
+
+
+# 对比前后的单词排名变化，从而得到趋势
+def compare_word_rank(file_pre, file_post, save_root, save_name):
+    print("Processing files: " + file_pre + "  and  " + file_post)
+    # 输入为两个词频表
+    word_freq_pre = pd.read_excel(file_pre)
+    word_freq_post = pd.read_excel(file_post)
+    # 获取rank
+    rank_cut = 200
+    pre_rank = get_rank(word_freq_pre['Frequency'], rank_cut)
+    post_rank = get_rank(word_freq_post['Frequency'], rank_cut)
+    # 转为字典
+    pre_rank_dict = store_ranks(word_freq_pre['Word'], pre_rank)
+    post_rank_dick = store_ranks(word_freq_post['Word'], post_rank)
+
+    # 从200以后截断，只考虑前两百
+    pre_PostRank = []               # 后面文件单词对应文件中的rank
+    rank_changeList = []            # rank的变化
+    dictkeys = list(post_rank_dick.keys())[:rank_cut]   # 只计算前rank_cut个数据的变化
+    for word in dictkeys:
+        pre_word_rank = pre_rank_dict.get(word)         # pre_rank, post_rank变量和之前的不同了
+        if pre_word_rank is None:
+            pre_word_rank = rank_cut
+        pre_PostRank.append(pre_word_rank)
+        post_word_rank = post_rank_dick.get(word)
+        rank_change = pre_word_rank - post_word_rank
+        rank_changeList.append(rank_change)
+    # 保存到文件中
+    save_rank_change(dictkeys, pre_PostRank, post_rank[:rank_cut], rank_changeList,
+                     save_root, save_name)
+
 
 if __name__ == '__main__':
+    count_frequency_flag = True
     paper_list_roots = ["D:\Papers\ECCV2022_Detect\statistics\eccv2022_all.xlsx",
                        "D:\Papers\CVPR2022_Detect\statistics\cvpr2022_all.xlsx"]          # 根目录
     # parameters for 词频统计
@@ -167,8 +248,18 @@ if __name__ == '__main__':
     wordcloud_filename = "ALL_CV2022_Word_Cloud"                # 保存词云图的文件名
 
     # 统计词频并绘图
-    count_word_frequency(paper_list_roots, stop_words_root, save_root,
-                         frequencies_filename, wordcloud_filename)
+    if count_frequency_flag:
+        count_word_frequency(paper_list_roots, stop_words_root, save_root,
+                             frequencies_filename, wordcloud_filename)
+
+    # 比较前后变化
+    word_rank_flag = False
+    WordFrequency_Files = ["D:\Papers\statistics\ALL_CV2022WordFrequency.xlsx",
+                           "D:\Papers\statistics\ALL_CV2023WordFrequency.xlsx"]
+    rank_compare_filename = "WordRankCompare2022To2023.xlsx"
+    if word_rank_flag:
+        compare_word_rank(WordFrequency_Files[0], WordFrequency_Files[1], save_root, rank_compare_filename)
+
 
 
 
